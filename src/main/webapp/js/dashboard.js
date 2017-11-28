0d86273c287624beb5e29378902b364a7d90c04b
luisfhz009 <32464611+luisfhz009@users.noreply.github.com>
2017-11-28 14:49:44 -0600

    actualice los html

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -1,129 +1,126 @@
-$(document).ready(function() {//Entra hasta que la pagine cargue
+$(document).ready( function(){ 
+  
+  var arregloDeObjetos=[];
+  var numeroProyecto = 1;
 
-	var arregloObjetos = []; //Guardar posts
-	var numeroPost = 0;
+//-----$ este signo indica que vamos a trabajar con el DOM (aquí buscamos un id #add-post especificando primero que esta dentro de un button)
+$("button#add-post")// este es el selector
+ // on espera una acción
+   .on( //metodo on
+        "click", // estos son los eventos que maneja el selector
+  function(event){ //Función manejadora
+                                                //.val trae los valores que se captan del boton textoPost en html (cuando el usuario escribe)a nuestra función
+              var postProyect   = $("input#proyect_name").val(); //devuelve un String
+            //var postEncargado = $("input#nombreEncargado").val();
+              var postDescrip   = $("textarea#proyect_description").val();
+              var postInicio    = $("input#proyect_start_date").val();
+              var postFinal     = $("input#proyect_final_date").val();
+              
+              postProyect   = $.trim(postProyect);// con esto (trim)quitamos los espacios en blanco a la hora de escribir en el post(para que no los tome como un post)
+            //postEncargado = $.trim(postEncargado);
+              postDescrip   = $.trim(postDescrip);
+              postInicio    = $.trim(postInicio);
+              postFinal     = $.trim(postFinal);
 
-	$("button#add-post").on(/*Selector*/"click"/*Evento a manejar*/, function (event) {/*Funcion manejadora*/
-		//console.log("Hiciste click");
-		var textArea = $("textarea#FormControlTextarea").val(); //Devuelve un string
-    textArea = $.trim(textArea);
-    //Comparacion si hay contenido que publicar
-    if (textArea !== ""){
-      //console.log("Estamos listos jQuery");
-    //console.log("Contenido", textArea);
-    //nuestro nuevo post
-    var nuevoPost = {
-      //numero: numeroPost = 1,
-      //numero: $("div.mi-post").length +1,
-      numero: $("div.mi-post").length,
-      contenido: textArea
-    };
+      if(postProyect !=='' && postDescrip !==''&& postInicio !==''&& postFinal !==''){ 
+      //nuestro nuevo post
+          var nuevoPost ={
+              numero:$('div.mi-post').length + 1,
+            //numero: numeroPost,
+              postProyect:postProyect,
+            //postEncargado:postEncargado,
+              postDescrip:postDescrip,
+              postInicio:postInicio,
+              postFinal:postFinal
+              };
+          //todo esto es poner la información que ya recibimos del usuario y ponerla en la pagina principal al mismo tiempo
 
-    console.log(nuevoPost);
-    console.log(arregloObjetos);
+                    //.prpend organiza la informacion del #post y la regresa al idex
+        $("div#post").append(//poner todo lo del html de la sección de post (todo esto es el template)
 
-    
-      //numeroPost ++;
-      //Obtienes contenido HTML
-      var html = $("div#posts").html();
-      //console.log(html);
-      //Editas contenido HTML
-      //$("div#posts").html("<h1>Publicacion</h1>");
-      //$("div#posts").prepend("<h1>Publicacion "+(arregloObjetos.length + 1)+"</h1>");
-      $("div#posts").prepend('<div id="mi-post-'+ nuevoPost.numero+'"class="container mi-post">'+
-        '<div class="row">'+
-          '<div class="col-md-2">'+
-          '</div>'+
-        '<!--Contenedor central con las fases de la publicacion-->'+
-        '<div class="col-md-8" id="com">'+
-          '<div class="row">'+
-            '<div class="col-md-2" id="pic">'+
-              '<i class="fa fa-cog fa-spin fa-5x fa-fw" aria-hidden="true"></i>'+
-            '</div>'+
-              '<div class="col-10">'+
-                '<div class="row">'+
-                  '<div class="col-5">'+
-                    '<h2>Ramon</h2>'+
-                    '<p></p>'+
-                    '<h4>Post #'+nuevoPost.numero+ '</h4>'+
-                  '</div>'+//col5
-                  '<div class="col-md-1 ml-auto">'+
-                    '<button class="btn btn-outline-danger btn-sm post-delete" data-numero=' + nuevoPost.numero + '>&times;</button>'+
+
+        '<div  id="mi-post' + nuevoPost.numero+'"class="col-3 mi-post proyectos">'+
+        
+        '<div class="title3">'+ /////TITULOS ////////////////////////////////////////////// -->
+        '<h5 class="title4"><b>Proyect Name:</b></br><span class="title2">'
+        +nuevoPost.postProyect+'</span></h5>'+ 
+        '<h5 class="title4"><b>Proyect :</b></br><span class="title2">'
+        +nuevoPost.numero+'</span></h5>'+
+                '</div>'+
+      
+                  '<div class="title3">'+ ////FECHAS DEL PROYECTO ////////////////////////////////////////////////-->
+                    '<div Style="float:left;">'+
+          '<h5 class="title4"><b>Start Date:</b></br>'+
+                      '<span class="title2">'+nuevoPost.postInicio+'</span></h5>'+
+                       '<h5 class="title4"><b>End Date:</b></br>'+
+                       '<span class="title2">'+nuevoPost.postFinal+'</span></h5>'+
+                    '</div>'+
                   '</div>'+
-                '</div>'+ //row
-              '</div>'+//col md 6
-            '</div>'+//row
-            '<div class="row justify-content-center">'+
-              '<p></p>'+
-            '</div>'+
-            '<div class="row" id="tex">'+
-              nuevoPost.contenido+
-            '</div>'+
-        '</div>'+
-          '<div class="col-md-2">'+
-          '</div>'+
-        '</div>'+
-      '</div>');
+      
+                    '<div class="title5">'+ //Estatus del Proyecto ///////////////////////////////// -->
+                      '<p class="title4">Description:</p>'+
+                      '<textarea class="title2">'+nuevoPost.postDescrip+'</textarea>'+
+                    '</div>'+
+        
+                    '<div buttons">'+ //boton para borrar proyecto /////////////////////////////// -->
+                      '<button class="btn btn-dark btn-sm post-delete" data-numero="'+ nuevoPost.numero +'">Drop</button>'+
+                      '<a href="task.html"><button class="btn btn-info btn-sm"style="float:right;">Admin</button></a>'+
+                    '</div>'+
+              '</div>'
 
-      //arregloObjetos[arregloObjetos.length] = nuevoPost;
+          );
 
-      //Limpiar el TextArea
-      $("textarea#FormControlTextarea").val("");
-      //Clickear button cancelar
-     // $("button#cancel-post").click();
-     //esconder el modal 
-      $("div#exampleModalLong").modal("hide");
-    //Si no hay nada escribe algo 
-    } else {
-      alert("Escribe tu publicacion");
-    }
-		
+          $("input#proyect_name").val(''); //esta ultima acción limpia el text area
+        //$("input#nombreEncargado").val('');
+          $("textarea#proyect_description").val('');
+          $("input#proyect_start_date").val('');
+          $("input#proyect_final_date").val('');
+           // esto ace referencia al boton de cerrar para que cuando clikemos en guardar tambien se cierre
+           // $("button#close-post").click(); //
+           //este otro es otro modo de hacer lo de cerrar la ventana
+           $("div#exampleModal").modal("hide");
 
-	});
-//Funcion para borrar publicacion
-  $("div#posts").on("click","button.post-delete", function (event) {
-    //console.log("Que le pacho");
-    //$("div.mi-post").remove();
-    //Saber cual boton presione y en que DIV
-    //$(this).parent();
-    //Confirmar la peticion de borrar
-    if (confirm("Estas seguro?")) {
-      //Nos traemos atributo del data-numero
-    var numero = $(this).data("numero");
-    //data-saludo
-    //var saludo = $(this).data("saludo");
-    var miId = "div#mi-post-"+ numero;
-    $(miId).remove();
-    }
-    //console.log("miId");
-  });
+      } 
+      else {
+          alert("Write Something!");
+
+          $("input#proyect_name").val(''); //esta acción limpia despues de que te muestra la alerta para que puedas escribir en limpio de nuevo
+        //$("input#nombreEncargado").val('');
+          $("textarea#proyect_description").val('');
+          $("input#proyect_start_date").val('');
+          $("input#proyect_final_date").val('');
+      }
+
+ });
+ //Creamos aquí otro evento para darle una función al botón que creamos para borrar un post
+ $("div#post" //En que contenedor lo vamos a buscar, en donde se in
+            ).on("click", //Evento a validar
+                         "button.post-delete", //Botón o elemento a validar
+                         function(event){ //Función manejadora
+                               //console.log("Te vas a ir!");
+                               //$("div.mi-post").remove();
+                               //$(this).parent();
+                        if(confirm("Are you shure?")){   
+                           var numero = $(this).data("numero");
+                           
+                           var miId = "div#mi-post" + numero;
+
+                           //console.log(miId);
+                           $(miId).remove();
+                        }
+
+ })
+ $("button#close-post" //En que contenedor lo vamos a buscar, en donde se in
+            ).on("click", //Evento a validar
+                         "button.close-post", //Botón o elemento a validar
+                         function(event){ //Función manejadora
+                          
+                          $("input#proyect_name").val(''); //esta ultima acción limpia el text area
+                        //$("input#nombreEncargado").val('');
+                          $("textarea#proyect_description").val('');
+                          $("input#proyect_start_date").val('');
+                          $("input#proyect_final_date").val('');             
+
+ })
 
 });
-
-//Prueba antes de meter a codigo el HTML
-/*'<div class="row">'+
-        '<div class="col-md-2">'+
-        '</div>'+
-        '<!--Contenedor central con las fases de la publicacion-->'+
-        '<div class="col-md-8" id="com">'+
-          '<div class="row">'+
-            '<div class="col-md-2" id="pic">'+
-              '<i class="fa fa-cog fa-spin fa-5x fa-fw" aria-hidden="true"></i>'+
-            '</div>'+
-            '<div class="col-md-6">'+
-              '<h2>Ramon</h2>'+
-            '<p></p>'+
-            '<h4>Post '+(arregloObjetos.length + 1)+ '</h4>'+
-            '</div>'+
-          '</div>'+
-          '<div class="row justify-content-center">'+
-            '<p></p>'+
-          '</div>'+
-          '<div class="row" id="tex">'+
-          nuevoPost.contenido+
-            '<p>Mi vida es bien divertida, Yupi!.</p>'+
-          '</div>'+
-        '</div>'+
-        '<div class="col-md-2">'+
-        '</div>'+
-      '</div>'*/
